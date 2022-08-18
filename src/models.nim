5e7b349ba4b471ba2e39cfe31c592ef1d93d11e3
nahuel facultad <nahuel.utn.frc.piguillem@gmail.com>
2022-08-18 15:30:19 -0300

    Primeros modelos, aprendiendo sobre norm

--- src/models.nim
+++ src/models.nim
@@ -1,11 +1,51 @@
 import norm/[model,sqlite]
+import std/with
 
 type
     Proyecto* = ref object of Model
-        nombre:string
-proc newProy*(n:string):Proyecto=
+        nombre*:string
+proc newProy*(n=""):Proyecto=
     Proyecto(nombre:n)
 
-let dbConn* = open(":memory:","","","")
-dbConn.createTables(newProy("P"))
-echo()
+type
+    UserStory* = ref object of Model
+        cod_proy*: int
+        nombre*:string
+
+proc newUserStory*(cod=0,n=""):UserStory=
+    UserStory(cod_proy:cod,nombre:n)
+
+type
+    TestCase* = ref object of Model
+        cod_user_story*: int
+        nombre*: string
+        expected*: string
+        resultado*:string
+        pass*:bool
+
+proc newTestCase*(cod=0,nombre="",exp="",res="",pass=false):TestCase=
+    TestCase(cod_user_story:cod,nombre:nombre,expected:exp,resultado:res,pass:pass)
+
+proc start_db*():DbConn=
+    var dbConn = open("database","","","")
+    dbConn.createTables(newProy())
+    dbConn.createTables(newUserStory())
+    dbConn.createTables(newTestCase())
+    return dbConn
+proc insert_proy*(dbConn:DbConn,p:var Proyecto)=
+    with dbConn:
+        insert p
+
+proc select_all_ps*(dbConn:DbConn,limit=50,offset=0):seq[Proyecto]=
+  var somePs = @[newProy()]
+  dbConn.select(somePs,"""TRUE LIMIT $1 OFFSET $2""",limit,offset)
+  return somePs
+
+proc select_all_user_story*(dbConn:DbConn,cod_proy:int,limit=50,offset=0):seq[UserStory]=
+    var someUss = @[newUserStory()]
+    dbConn.select(someUss,"""cod_proy = $1 LIMIT $2 OFFSET $3""",cod_proy,limit,offset)
+    return someUss
+
+proc insert_us*(dbConn:DbConn,us:var UserStory)=
+    with dbConn:
+        insert us
--- src/test_case_app.nim
+++ src/test_case_app.nim
@@ -1,28 +1,79 @@
 import norm/[model,sqlite]
 import std/with
 import std/os
-removeFile("database")
-type
-    Proyecto* = ref object of Model
-        nombre:string
+import models
+
+proc restart_data_base()=
+  removeFile("database")
+proc play()=
+  restart_data_base()
+  type
+      Proyecto = ref object of Model
+          nombre:string
 
 
-proc newProy*(n=""):Proyecto=
-    Proyecto(nombre:n)
+  proc newProy(n=""):Proyecto=
+      Proyecto(nombre:n)
 
-let dbConn* = open("database","","","")
-dbConn.createTables(newProy())
-var p=newProy("testing")
-var p2=newProy("mas testing")
-with dbConn:
-  insert p
-  insert p2
-var somePs = @[newProy()]
-dbConn.select(somePs,"""TRUE LIMIT $1 OFFSET $2""",10,0)
+  let dbConn = open("database","","","")
+  
+  dbConn.createTables(newProy())
 
-for p in somePs:
-  echo p[]
+  
+  var p=newProy("testing")
+  var p2=newProy("mas testing")
+  with dbConn:
+    insert p
+    insert p2
+  var somePs = @[newProy()]
+  dbConn.select(somePs,"""TRUE LIMIT $1 OFFSET $2""",10,0)
+  
+  for p in somePs:
+    echo p[]
 
-echo()
+  echo()
 
-echo("Bienvenido al test case app")
+proc main_point()=
+  restart_data_base()
+  var dbConn=start_db()
+  
+  var p1 = newProy("Nafa")
+  var p2 = newProy("algo")
+  insert_proy(dbConn,p1)
+  insert_proy(dbConn,p2)
+  #with dbConn:
+  #  insert p1
+  #  insert p2
+  #var somePs = @[newProy()]
+  #dbConn.select(somePs,"""TRUE LIMIT $1 OFFSET $2""",10,0)
+  var somePs=select_all_ps(dbConn)
+  for p in somePs:
+    echo p[]
+
+  var us1=newUserStory(1,"probando")
+  var us2=newUserStory(1,"testeando")
+  var us3=newUserStory(2,"looking")
+
+  insert_us(dbConn,us1)
+  insert_us(dbConn,us2)
+  insert_us(dbConn,us3)
+
+  var someUss1=select_all_user_story(dbConn,1)
+  echo "proy 1"
+  for u in someUss1:
+    echo u[]
+  
+  var someUss2=select_all_user_story(dbConn,2)
+  echo "proy 2"
+  for u in someUss2:
+    echo u[]
+
+  echo()
+when isMainModule:
+
+  echo("Bienvenido al test case app")
+  main_point()
+
+
+
+
